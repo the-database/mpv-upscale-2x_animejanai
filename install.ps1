d0513c063f48f8577c93003070c2206070236d8e
the-database <25811902+the-database@users.noreply.github.com>
2022-11-07 10:12:05 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # Download doeverything.ps1
 Import-Module BitsTransfer
-$download = "https://raw.githubusercontent.com/the-database/mpv-upscale/main/doeverything.ps1"
 $script = "doeverything.ps1"
+$download = "https://raw.githubusercontent.com/the-database/mpv-upscale/main/$script"
 Start-BitsTransfer -Source $download -Destination $script
 
 # Run doeverything.ps1
