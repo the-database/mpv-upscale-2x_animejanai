01e46f29a785981dd022c05c61fc8bb3615f14b8
the-database <25811902+the-database@users.noreply.github.com>
2023-03-12 19:29:30 -0400

    refactor v2 vpy progress

--- shaders/animejanai_v2.py
+++ shaders/animejanai_v2.py
@@ -1,5 +1,14 @@
 import vapoursynth as vs
-import os, subprocess, logging
+import os
+import subprocess
+import logging
+import configparser
+import itertools
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import rife_cuda
 
 # trtexec num_streams
 TOTAL_NUM_STREAMS = 4
@@ -7,126 +16,192 @@ TOTAL_NUM_STREAMS = 4
 core = vs.core
 core.num_threads = 4  # can influence ram usage
 
-plugin_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
-   "..\\..\\vapoursynth64\\plugins\\vsmlrt-cuda")
+plugin_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                           r"..\..\vapoursynth64\plugins\vsmlrt-cuda")
 
-
-# create logger with 'animejanai_v2'
 formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
-                                  datefmt='%Y-%m-%d %H:%M:%S')
+                              datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger('animejanai_v2')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'animejanai_v2.log'))
-fh.setFormatter(formatter)
-fh.setLevel(logging.DEBUG)
-logger.addHandler(fh)
+
+config = {}
+
+
+def init_logger():
+    global logger
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'animejanai_v2.log'))
+    fh.setFormatter(formatter)
+    fh.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
+
+
+def read_config():
+    dummykey = 'config'
+    bools = {
+        'logging',
+        *{f'upscale_4x_{i}' for i in range(1, 10)},
+        *{f'resize_720_to_1080_before_first_2x_{i}' for i in range(1, 10)},
+        *{f'upscale_2x_{i}' for i in range(1, 10)},
+        *{f'resize_to_1080_before_second_2x_{i}' for i in range(1, 10)},
+        *{f'rife_{i}' for i in range(1, 10)}
+    }  # TODO
+    floats = {f'resize_factor_before_first_2x_{i}' for i in range(1, 10)} | \
+             {f'resize_height_before_first_2x_{i}' for i in range(1, 10)}
+
+    parser = configparser.ConfigParser()
+    conf = {}
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "./animejanai_v2.conf")) as lines:
+        lines = itertools.chain((f"[{dummykey}]",), lines)
+        parser.read_file(lines)
+
+    for key in parser[dummykey]:
+        if key in bools:
+            conf[key] = True if parser[dummykey][key].casefold() == 'yes'.casefold() else False
+        elif key in floats:
+            conf[key] = float(parser[dummykey][key])
+        else:
+            conf[key] = parser[dummykey][key]
+
+    return conf
 
 
 # model_type: HD or SD
 # binding: 1 through 9
 def find_model(model_type, binding):
-   for filename in os.listdir(plugin_path):
-      if filename.endswith('.onnx') and f"{model_type}-{binding}" in filename:
-         return filename.replace('.onnx', '')
-   return None
+    key = f'{model_type.lower()}_model_{binding}'
+    if key in config:
+        return config[key]
+    return None
 
 
 def create_engine(onnx_name):
-   onnx_path = os.path.join(plugin_path, f"{onnx_name}.onnx")
-   if not os.path.isfile(onnx_path):
-      raise FileNotFoundError(onnx_path)
+    onnx_path = os.path.join(plugin_path, f"{onnx_name}.onnx")
+    if not os.path.isfile(onnx_path):
+        raise FileNotFoundError(onnx_path)
 
-   subprocess.run([os.path.join(plugin_path, "trtexec"), "--fp16", f"--onnx={onnx_name}.onnx",
-      "--minShapes=input:1x3x8x8", "--optShapes=input:1x3x1080x1920", "--maxShapes=input:1x3x1080x1920",
-      f"--saveEngine={onnx_name}.engine", "--tacticSources=+CUDNN,-CUBLAS,-CUBLAS_LT"], 
-      cwd=plugin_path)
+    subprocess.run([os.path.join(plugin_path, "trtexec"), "--fp16", f"--onnx={onnx_name}.onnx",
+                    "--minShapes=input:1x3x8x8", "--optShapes=input:1x3x1080x1920", "--maxShapes=input:1x3x1080x1920",
+                    f"--saveEngine={onnx_name}.engine", "--tacticSources=+CUDNN,-CUBLAS,-CUBLAS_LT"],
+                   cwd=plugin_path)
 
 
 def scale_to_1080(clip, w=1920, h=1080):
-   if clip.width / clip.height > 16 / 9:
-      prescalewidth = w
-      prescaleheight = w * clip.height / clip.width
-   else:
-      prescalewidth = h * clip.width / clip.height
-      prescaleheight = h
-   return vs.core.resize.Bicubic(clip, width=prescalewidth, height=prescaleheight)
+    if clip.width / clip.height > 16 / 9:
+        prescalewidth = w
+        prescaleheight = w * clip.height / clip.width
+    else:
+        prescalewidth = h * clip.width / clip.height
+        prescaleheight = h
+    return vs.core.resize.Bicubic(clip, width=prescalewidth, height=prescaleheight)
 
 
 def upscale2x(clip, sd_engine_name, hd_engine_name, num_streams):
-   engine_name = sd_engine_name if clip.height < 720 else hd_engine_name
-   engine_path = os.path.join(plugin_path, f"{engine_name}.engine")
+    engine_name = sd_engine_name if clip.height < 720 else hd_engine_name
+    engine_path = os.path.join(plugin_path, f"{engine_name}.engine")
 
-   message = f"upscale2x: scaling from {clip.width}x{clip.height} with engine={engine_name}; num_streams={num_streams}"
-   logger.debug(message)
-   print(message)
+    message = f"upscale2x: scaling 2x from {clip.width}x{clip.height} with engine={engine_name}; num_streams={num_streams}"
+    logger.debug(message)
+    print(message)
 
-   if not os.path.isfile(engine_path):
-      create_engine(engine_name)
+    if not os.path.isfile(engine_path):
+        create_engine(engine_name)
 
-   return core.trt.Model(
-      clip,
-      engine_path=engine_path,
-      num_streams=num_streams,
-   )
+    return core.trt.Model(
+        clip,
+        engine_path=engine_path,
+        num_streams=num_streams,
+    )
 
 
-def run_animejanai(clip, sd_engine_name, hd_engine_name):
-   colorspace="709"
-   colorlv = clip.get_frame(0).props._ColorRange
-   fmt_in = clip.format.id
+def run_animejanai(clip, clip_dw, clip_dh, container_fps, sd_engine_name, hd_engine_name, resize_factor_before_first_2x,
+                   resize_height_before_first_2x, resize_720_to_1080_before_first_2x, do_upscale,
+                   resize_to_1080_before_second_2x, upscale_twice, use_rife):
+    if do_upscale:
+        colorspace = "709"
+        colorlv = clip.get_frame(0).props._ColorRange
+        fmt_in = clip.format.id
 
-   if clip.height < 720:
-      colorspace = "170m"
+        if clip.height < 720:
+            colorspace = "170m"
 
-   clip = vs.core.resize.Bicubic(clip, format=vs.RGBS, matrix_in_s=colorspace,
-      # width=clip.width/2.25,height=clip.height/2.25 # pre-downscale
-      )
+        if resize_height_before_first_2x != 0:
+            resize_factor_before_first_2x = 1
 
-   # pre-scale 720p or higher to 1080
-   if clip.height >= 720 or clip.width >= 1280:
-      clip = scale_to_1080(clip)
+        clip = vs.core.resize.Bicubic(clip, format=vs.RGBS, matrix_in_s=colorspace,
+                                      width=clip.width/resize_factor_before_first_2x,
+                                      height=clip.height/resize_factor_before_first_2x)
 
-   upscale_twice = clip.height < 1080 and clip.width < 1920
-   num_streams = TOTAL_NUM_STREAMS
-   if upscale_twice:
-      num_streams /= 2
+        if resize_height_before_first_2x != 0:
+            clip = scale_to_1080(clip, resize_height_before_first_2x * 16 / 9, resize_height_before_first_2x)
 
-   # upscale 2x
-   clip = upscale2x(clip, sd_engine_name, hd_engine_name, num_streams)
+        # pre-scale 720p or higher to 1080
+        if resize_720_to_1080_before_first_2x:
+            if (clip.height >= 720 or clip.width >= 1280) and clip.height < 1080 and clip.width < 1920:
+                clip = scale_to_1080(clip)
 
-   # upscale 2x again if necessary
-   if upscale_twice:
+        upscale_twice = upscale_twice and clip.height < 1080 and clip.width < 1920
+        num_streams = TOTAL_NUM_STREAMS
+        if upscale_twice:
+            num_streams /= 2
 
-      # downscale down to 1080 if first 2x went over 1080
-      if clip.height > 1080 or clip.width > 1920:
-         clip = scale_to_1080(clip)
+        # upscale 2x
+        clip = upscale2x(clip, sd_engine_name, hd_engine_name, num_streams)
 
-      # add slight blur before second upscale, since model expects slight blur
-      # this helps prevent oversharpening when upscaling twice
-      clip = core.std.BoxBlur(clip)
-      clip = core.std.BoxBlur(clip)
+        # upscale 2x again if necessary
+        if upscale_twice:
+            # downscale down to 1080 if first 2x went over 1080,
+            # or scale up to 1080 if enabled
+            if resize_to_1080_before_second_2x or clip.height > 1080 or clip.width > 1920:
+                clip = scale_to_1080(clip)
 
-      # upscale 2x again
-      clip = upscale2x(clip, sd_engine_name, hd_engine_name, num_streams)
+            # upscale 2x again
+            clip = upscale2x(clip, sd_engine_name, hd_engine_name, num_streams)
 
-   fmt_out = fmt_in
-   if fmt_in not in [vs.YUV410P8, vs.YUV411P8, vs.YUV420P8, vs.YUV422P8, vs.YUV444P8, vs.YUV420P10, vs.YUV422P10, vs.YUV444P10] :
-      fmt_out = vs.YUV420P10
+        fmt_out = fmt_in
+        if fmt_in not in [vs.YUV410P8, vs.YUV411P8, vs.YUV420P8, vs.YUV422P8, vs.YUV444P8, vs.YUV420P10, vs.YUV422P10,
+                          vs.YUV444P10]:
+            fmt_out = vs.YUV420P10
 
-   clip = vs.core.resize.Bicubic(clip, format=fmt_out, matrix_s=colorspace, range=1 if colorlv==0 else None)
-   clip.set_output()
+        clip = vs.core.resize.Bicubic(clip, format=fmt_out, matrix_s=colorspace, range=1 if colorlv == 0 else None)
+
+    if use_rife:
+        clip = rife_cuda.rife(clip, clip.width, clip.height, container_fps)
+
+    clip.set_output()
+
 
 # keybinding: 1-9
-def run_animejanai_with_keybinding(clip, keybinding):
+def run_animejanai_with_keybinding(clip, clip_dw, clip_dh, container_fps, keybinding):
+    sd_engine_name = find_model("SD", keybinding)
+    hd_engine_name = find_model("HD", keybinding)
+    do_upscale = config.get(f'upscale_2x_{keybinding}', True)
+    upscale_twice = config.get(f'upscale_4x_{keybinding}', True)
+    use_rife = config.get(f'rife_{keybinding}', False)
+    resize_720_to_1080_before_first_2x = config.get(f'resize_720_to_1080_before_first_2x_{keybinding}', True)
+    resize_factor_before_first_2x = config.get(f'resize_factor_before_first_2x_{keybinding}', 1)
+    resize_height_before_first_2x = config.get(f'resize_height_before_first_2x_{keybinding}', 0)
+    resize_to_1080_before_second_2x = config.get(f'resize_to_1080_before_second_2x_{keybinding}', True)
 
-   sd_engine_name = find_model("SD", keybinding)
-   hd_engine_name = find_model("HD", keybinding)
+    if do_upscale:
+        if sd_engine_name is None:
+            raise FileNotFoundError(
+                f"No SD model found for keybinding ctrl+{keybinding}. Expected to find an onnx model with filename containing 'SD-{keybinding}' in the following path: {plugin_path}")
 
-   if sd_engine_name is None:
-      raise FileNotFoundError(f"No SD model found for keybinding ctrl+{keybinding}. Expected to find an onnx model with filename containing 'SD-{keybinding}' in the following path: {plugin_path}")
+        if hd_engine_name is None:
+            raise FileNotFoundError(
+                f"No HD model found for keybinding ctrl+{keybinding}. Expected to find an onnx model with filename containing 'HD-{keybinding}' in the following path: {plugin_path}")
 
-   if hd_engine_name is None:
-      raise FileNotFoundError(f"No HD model found for keybinding ctrl+{keybinding}. Expected to find an onnx model with filename containing 'HD-{keybinding}' in the following path: {plugin_path}")
+    run_animejanai(clip, clip_dw, clip_dh, container_fps, sd_engine_name, hd_engine_name, resize_factor_before_first_2x,
+                   resize_height_before_first_2x, resize_720_to_1080_before_first_2x, do_upscale,
+                   resize_to_1080_before_second_2x, upscale_twice, use_rife)
 
-   run_animejanai(clip, sd_engine_name, hd_engine_name)
+
+def init():
+    global config
+    config = read_config()
+    if config['logging']:
+        init_logger()
+
+
+init()
